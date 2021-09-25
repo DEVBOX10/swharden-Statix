d0c2a4881f22f47ee92ad2112f02e4161472375f
Scott W Harden <swharden@gmail.com>
2021-09-25 17:29:36 -0400

    update local build script

--- build.bat
+++ build.bat
@@ -4,4 +4,4 @@ dotnet run ^
  --content sample/content ^
  --theme sample/themes/statixdemo ^
  --urlSource https://github.com/swharden/Statix/blob/main/sample/content ^
- --urlSite http://localhost:8080
+ --urlSite http://localhost:8080/sample-site
