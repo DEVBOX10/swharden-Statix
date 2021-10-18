cfcb64df6cb0ae1c4e0f7f18ac37ff8834751fa1
Scott W Harden <swharden@gmail.com>
2021-10-17 21:05:32 -0400

    Tests: FTP deployment and sync file

---
+++ src/StatixTests/DeployTests/FtpUploaderTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using Statix.Deploy;
+
+namespace StatixTests.DeployTests
+{
+    internal class FtpUploaderTests
+    {
+        private static (string user, string pass) GetLoginCreds()
+        {
+            // load secrets into environment variables
+            var config = new ConfigurationBuilder().AddUserSecrets<FtpUploaderTests>().Build();
+            foreach (var child in config.GetChildren())
+                Environment.SetEnvironmentVariable(child.Key, child.Value);
+
+            // read secrets out of environment variables
+            string? user = Environment.GetEnvironmentVariable("SANDBOX_FTP_USERNAME");
+            string? pass = Environment.GetEnvironmentVariable("SANDBOX_FTP_PASSWORD");
+
+            // throw if secrets not loaded
+            if (user is null)
+                throw new InvalidOperationException("environment variables do not contain username");
+            if (pass is null)
+                throw new InvalidOperationException("environment variables do not contain password");
+
+            return (user, pass);
+        }
+
+        [Test]
+        public void Test_Uploader()
+        {
+            // https://swharden.com/dev/sandbox/sample/folder/image.jpg
+
+            Plan plan = new();
+            plan.AddFile(SampleFile.IMAGE_PATH, "/sample/folder/image.jpg");
+
+            (string user, string pass) = GetLoginCreds();
+            using var uploader = new FtpUploader("swharden.com", user, pass);
+
+            uploader.RemoteDeploy(plan);
+        }
+    }
+}
---
+++ src/StatixTests/DeployTests/PlanTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Statix.Deploy;
+
+namespace StatixTests.DeployTests
+{
+    internal class PlanTests
+    {
+        [Test]
+        public void Test_Plan_AddFile()
+        {
+            Plan plan = new();
+            plan.AddFile(SampleFile.IMAGE_PATH, "/sample/file/file1.jpg");
+            Assert.AreEqual(1, plan.FileCount);
+        }
+
+        [Test]
+        public void Test_Plan_AddFolder()
+        {
+            Plan plan = new();
+            plan.AddFolder(SampleFile.FOLDER_PATH, "/sample/folder/");
+            Assert.Greater(plan.FileCount, 1);
+        }
+
+        [Test]
+        public void Test_Plan_ToJson()
+        {
+            Plan plan = new();
+            plan.AddFolder(SampleFile.FOLDER_PATH, "/sample/folder/");
+
+            var uploader = new MockUploader();
+            uploader.RemoteDeploy(plan);
+
+            string json = plan.ToJson();
+            Console.WriteLine(json);
+            Assert.IsNotNull(json);
+            Assert.IsNotEmpty(json);
+        }
+
+        [Test]
+        public void Test_Plan_FromJson()
+        {
+            var uploader = new MockUploader();
+
+            Plan plan1 = new();
+            plan1.AddFolder(SampleFile.FOLDER_PATH, "/sample/folder/");
+            uploader.RemoteDeploy(plan1);
+            string json = plan1.ToJson();
+
+            Plan plan2 = new(json);
+            plan2.AddFolder(SampleFile.FOLDER_PATH, "/sample/folder/");
+            uploader.RemoteDeploy(plan2);
+        }
+    }
+}
