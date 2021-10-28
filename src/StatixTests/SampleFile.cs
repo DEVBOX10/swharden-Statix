b97e5901a81069cd4fc76ac80064be3052a4dab3
Scott W Harden <swharden@gmail.com>
2021-10-27 22:49:16 -0400

    Tests: detect repo root path in the cloud

--- src/StatixTests/SampleFile.cs
+++ src/StatixTests/SampleFile.cs
@@ -10,9 +10,27 @@ namespace StatixTests
 {
     internal static class SampleFile
     {
-        public static string REPO_ROOT = Path.GetFullPath(Path.Combine(TestContext.CurrentContext.TestDirectory, "../../../../../"));
+        public static string REPO_ROOT
+        {
+            get
+            {
+                string localPath = Path.GetFullPath(Path.Combine(TestContext.CurrentContext.TestDirectory, "../../../../../"));
+                if (Directory.Exists(localPath))
+                    return localPath;
+                else
+                    Console.WriteLine($"repo root is not: {localPath}");
 
-        public static string FOLDER_PATH = Path.Combine(REPO_ROOT, "sample/content/");
+                string cloudRunnerRoot = "/home/runner/work/Statix/Statix";
+                if (Directory.Exists(cloudRunnerRoot))
+                    return cloudRunnerRoot;
+                else
+                    Console.WriteLine($"repo root is not: {cloudRunnerRoot}");
+
+                throw new InvalidOperationException("cannot locate repo root");
+            }
+        }
+
+        public static string FOLDER_PATH => Path.Combine(REPO_ROOT, "sample/content/");
 
         public static string IMAGE_PATH = Path.Combine(REPO_ROOT, "sample/content/images/small.jpg");
         public static string IMAGE_HASH = "43becb771f2eebcb72196bca905ad3ba";
